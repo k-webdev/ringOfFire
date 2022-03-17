d085ec88ea7f88406a1ff1131a16ae5a98db5b0b
Dennis Kemnade <Dennis.Kemnade@live.de>
2022-03-17 12:19:25 +0100

    addet the start-screen and the game.component

---
+++ src/app/start-screen/start-screen.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-start-screen',
+  templateUrl: './start-screen.component.html',
+  styleUrls: ['./start-screen.component.scss']
+})
+export class StartScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+newGame(){
+  
+}
+
+}
