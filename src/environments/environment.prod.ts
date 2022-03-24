504df14e41756a3cbad32ee18524ceaad4449dbe
1987DK1987 <87419838+1987DK1987@users.noreply.github.com>
2022-03-24 17:31:51 +0100

    enviroment.prod.ts updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDQRL4j7yXuTB1jpPjDoPr5hwDfAIB33fw",
+  authDomain: "ring-of-fire-55a75.firebaseapp.com",
+  projectId: "ring-of-fire-55a75",
+  storageBucket: "ring-of-fire-55a75.appspot.com",
+  messagingSenderId: "373534217504",
+  appId: "1:373534217504:web:c315dfb5065568bf46b345"
+  }
 };
